806c8b1d9db725422f6f9da6e6c89c2b6bdbda48
Ulises <uliisesrodriguez809@gmail.com>
2024-06-25 19:57:10 -0300

    estructura de carpetas y archivos base creados

---
+++ src/components/Auth/PasswordReset.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PasswordReset = () => {
+    return (
+        <div>PasswordReset</div>
+    )
+}
---
+++ src/components/Events/EventForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EventForm = () => {
+    return (
+        <div>EventForm</div>
+    )
+}
