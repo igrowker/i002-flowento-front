1628e315ea2cf0efd394826a1e394f55aef1bc87
ChristianGil <ldconesa@gmail.com>
2024-06-29 18:30:25 -0300

    login y navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Login from './components/Auth/Login.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar/>
+    <Login/>
     <App />
   </React.StrictMode>,
 )
