4aeacbc29e8787aa5d93a9a366f35a6d8ba4843f
Ulises <uliisesrodriguez809@gmail.com>
2024-06-29 21:24:13 -0300

    avances en el form de registro

---
+++ src/components/Auth/Form.jsx
@@ -0,0 +1,14 @@
+export const Form = () => {
+    return (
+        <form className=" mt-36 font-lato flex flex-col">
+            <div className="my-2.5 flex justify-center items-center flex-col">
+                <label htmlFor="nombre" className="w-full mb-2.5 leading-3 font-normal text-gray300">Nombre <span className="text-redprimary">*</span></label>
+                <input type="text" className="w-full px-3 h-8 border-solid rounded border-2 border-orangeprimary" id="nombre" name="nombre" placeholder="Nombre"/>
+            </div>
+            <div className="my-2.5 flex justify-center items-center flex-col">
+                <label htmlFor="apellido" className="w-full mb-2.5 leading-3 font-normal text-gray300">Apellido <span className="text-redprimary">*</span></label>
+                <input type="text" className="w-full px-3 h-8 border-solid rounded border-2 border-orangeprimary" id="apellido" name="apellido" placeholder="Apellido"/>
+            </div>
+        </form>
+    )
+}
--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import btnAtrasImg from "../../assets/Boton-Atras.png";
+import avatarImg from "../../assets/Avatar.png";
+import btnEditImg from "../../assets/Boton-Edit.png";
+import { Form } from './Form';
+
+// https://stackoverflow.com/questions/37457128/react-open-file-browser-on-click-a-div --> esto para q cuando se le de click a la imagen del editar imagen del avatar
 
 export const Register = () => {
     return (
-        <div>Register</div>
+        <>
+            {/* este margin provicional xq como no esta completa la vista del register en figma nose si el footer se tiene q ver o no */}
+            <div className='mb-56'>
+                <div className='bg-gradient-red'>
+                    <div className='ml-5 min-h-11 inset-y-10 flex items-center'>
+                        <img src={btnAtrasImg} alt='boton volver al login' />
+                    </div>
+                </div>
+                <div className='bg-orangesecondary  max-h-orangeModal'>
+                    <div className='flex justify-center items-center flex-col'>
+                        <div>
+                            <img src={avatarImg} alt='imagen avatar usuario' className='relative top-8' />
+                        </div>
+                        <div>
+                            <img src={btnEditImg} alt='imagen boton editar avatar' className='relative top-2' />
+                            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" className='hidden' />
+                        </div>
+                    </div>
+                </div>
+                <div className='container mx-auto px-6'>
+                    <Form />
+                </div>
+            </div>
+        </>
     )
 }
