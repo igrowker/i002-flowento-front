9b19759b2ecda10726c8befce3578d053c8291ef
Ulises <uliisesrodriguez809@gmail.com>
2024-06-29 21:27:02 -0300

    avances en la vista registro

--- src/components/Auth/Form.jsx
+++ src/components/Auth/Form.jsx
@@ -9,6 +9,10 @@ export const Form = () => {
                 <label htmlFor="apellido" className="w-full mb-2.5 leading-3 font-normal text-gray300">Apellido <span className="text-redprimary">*</span></label>
                 <input type="text" className="w-full px-3 h-8 border-solid rounded border-2 border-orangeprimary" id="apellido" name="apellido" placeholder="Apellido"/>
             </div>
+            <div className="my-2.5 flex justify-center items-center flex-col">
+                <label htmlFor="email" className="w-full mb-2.5 leading-3 font-normal text-gray300">Email <span className="text-redprimary">*</span></label>
+                <input type="text" className="w-full px-3 h-8 border-solid rounded border-2 border-orangeprimary" id="email" name="email" placeholder="example@gmeil.com"/>
+            </div>
         </form>
     )
 }
