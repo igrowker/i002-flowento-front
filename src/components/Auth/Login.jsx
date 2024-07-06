b186d8a1f077bac347e2ac9015d3acd8e34cc22a
Ulises <uliisesrodriguez809@gmail.com>
2024-07-06 15:10:03 -0300

    seteo de cookies funcionando

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,8 +1,46 @@
 import "tailwindcss/tailwind.css";
 import logow from "../../assets/logow.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useRef } from "react";
 
 function Login() {
+
+  const form = useRef(null);
+
+  const login = (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form.current);
+
+    const obj = {};
+
+    data.forEach((value, key) => obj[key] = value);
+
+    //en el front no olvidar esta parte
+    axios.defaults.withCredentials = true;
+
+    //cuando lo montemos en vercel o render estas url hay q cambiarlas
+    axios.post("http://localhost:8080/auth/login", {
+      email: obj["email"],
+      password: obj["password"]
+    })
+      .then(response => {
+        const { data } = response;
+
+        //deje algunos console log para q vean q les llega desde el back
+        console.log(response);
+        console.log(data);
+
+        if (data.status === "success") {
+          alert("Te logueaste con exito");
+        }
+        else {
+          alert("alguno de los datos es incorrecto"); //en la data el mensaje puede ser mas perzonalizado
+        }
+      })
+  }
+
   return (
     <>
       <div className="flex min-h-full flex-col font-sans  justify-center px-6 py-12 lg:px-8">
@@ -18,7 +56,7 @@ function Login() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form ref={form} className="space-y-6" onSubmit={(e) => login(e)}>
             <div>
               <label
                 htmlFor="email"
@@ -32,7 +70,7 @@ function Login() {
                   id="email"
                   name="email"
                   type="email"
-                  autocomplete="email"
+                  autoComplete="email"
                   placeholder=" Ingrese su email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
@@ -64,7 +102,7 @@ function Login() {
                   id="password"
                   name="password"
                   type="password"
-                  autocomplete="current-password"
+                  autoComplete="current-password"
                   placeholder=" Ingrese su contraseña"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-100 sm:text-sm sm:leading-6"
@@ -85,7 +123,7 @@ function Login() {
           <p className="mt-10 text-center text-sm text-gray-500">
             No tienes Cuenta?
             <Link to={"/register"}>
-              <span className='className="font-semibold leading-6 text-orangeprimary hover:text-orangesecondary"'>
+              <span className="font-semibold leading-6 text-orangeprimary hover:text-orangesecondary">
                 Registrate
               </span>
             </Link>
