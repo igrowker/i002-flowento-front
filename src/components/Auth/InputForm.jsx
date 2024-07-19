6aea02d737df812194f986c72522c577b0093729
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-07-19 04:16:35 -0300

    input form

--- src/components/Auth/InputForm.jsx
+++ src/components/Auth/InputForm.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable react/prop-types */
-import { InputPassword } from './InputPassword';
+import { InputPassword } from "./InputPassword";
 
 export const InputForm = ({ name, type, required, description }) => {
-
   return (
     <div className="my-2.5 flex justify-center items-center flex-col">
-      <label htmlFor={name.toLowerCase()} className="w-full mb-2.5 leading-3 font-normal text-gray300">
+      <label
+        htmlFor={name.toLowerCase()}
+        className="w-full mb-2.5 leading-3 font-normal text-gray300"
+      >
         {name} {required && <span className="text-redprimary">*</span>}
       </label>
-      {
-        type === "password" ? <InputPassword name={name} />
-          : <div className='h-9 w-full'>
-            <input type={type} className="w-full px-3 h-full border-solid rounded border-2 border-orangeprimary" id={name.toLowerCase()} name={name.toLowerCase()} placeholder={name} />
-          </div>
-      }
-      {description !== "" && <p className='text-bghours w-full'>{description}</p>}
-    </div>)
-}
+      {type === "password" ? (
+        <InputPassword name={name} />
+      ) : (
+        <div className="w-full h-9">
+          <input
+            type={type}
+            className="w-full h-full px-3 border-2 border-solid rounded border-orangeprimary"
+            id={name.toLowerCase()}
+            name={name.toLowerCase()}
+            placeholder={name}
+          />
+        </div>
+      )}
+      {description !== "" && (
+        <p className="w-full text-bghours">{description}</p>
+      )}
+    </div>
+  );
+};
