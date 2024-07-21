f771636879e1a39c7d20f71ee40cbd4de9fb74a5
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-07-20 22:00:18 -0300

    detalles del perfil

--- src/components/Auth/InputPerfil.jsx
+++ src/components/Auth/InputPerfil.jsx
@@ -1,71 +1,86 @@
-import imgAvatar from "../../assets/Avatar.png";
 import { Link } from "react-router-dom";
-import "tailwindcss/tailwind.css";
+import imgAvatar from "../../assets/Avatar.png";
+import { FaPhone, FaUser } from "react-icons/fa";
+import { BsCalendar2Check } from "react-icons/bs";
+import { CgMail } from "react-icons/cg";
+import { GoLocation } from "react-icons/go";
+import { GrUserWorker } from "react-icons/gr";
+import { MdOutlineMapsHomeWork } from "react-icons/md";
+import { IoInformation } from "react-icons/io5";
+import PropTypes from "prop-types";
+
+const InfoRow = ({ icon: Icon, label, value }) => (
+  <>
+    <span className="flex items-center text-sm font-bold text-orangeprimary">
+      <Icon className="mr-2" />
+      <span>{label}:</span>
+    </span>
+    <span className="text-sm text-gray-900">{value}</span>
+  </>
+);
+
+InfoRow.propTypes = {
+  icon: PropTypes.elementType.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 const InputPerfil = () => {
+  const userInfo = [
+    { icon: FaUser, label: "Nombre y Apellido", value: "Nombre Apellido" },
+    {
+      icon: BsCalendar2Check,
+      label: "Fecha de Nacimiento",
+      value: "04/02/2004",
+    },
+    { icon: FaPhone, label: "Teléfono", value: "+54 11 12345678" },
+    { icon: CgMail, label: "Email", value: "usuario@gmail.com" },
+    { icon: GoLocation, label: "Dirección", value: "Buenos Aires" },
+    { icon: GrUserWorker, label: "Cargo", value: "Programador" },
+    { icon: MdOutlineMapsHomeWork, label: "Empresa", value: "Igrowker" },
+    {
+      icon: IoInformation,
+      label: "Información",
+      value:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo architecto, saepe suscipit quos maiores tempore!",
+    },
+  ];
+
   return (
     <>
-      <div className="mb-56 font-lato">
+      <div className="flex content-center justify-center font-lato">
         <div className="relative flex flex-col items-center">
-          <div className="text-4xl bg-White h-80 font-lato">
-            <div className="relative flex flex-col items-center justify-center top-16">
-              <p>Nombre y Apellido</p>
+          <div className="my-4 overflow-hidden bg-white rounded-lg">
+            <div className="flex flex-col items-center justify-center top-16">
+              <span className="my-4 text-2xl font-bold text-center text-redprimary">
+                Nombre del Usuario
+              </span>
               <img src={imgAvatar} alt="imagen Perfil" />
-              <p>Fecha de nacimiento:</p>
+            </div>
+            <div className="px-6 pt-5 text-center lg:px-96">
+              <div className="grid grid-cols-2 gap-2 py-1 md:gap-4 sm:px-6">
+                {userInfo.map((info, index) => (
+                  <InfoRow key={index} {...info} />
+                ))}
+              </div>
             </div>
           </div>
 
-          <Link to={"/perfil-edit"}>
-            <span className="items-center font-semibold leading-6 text-orangeprimary hover:text-orangesecondary">
-              Editar perfil
-            </span>
-          </Link>
-        </div>
-
-        <div className="max-w-md p-4 mx-auto ">
-          <div>
-            <input
-              type="text"
-              placeholder="Cargo"
-              className="w-full p-2 mb-2 border rounded"
-            />
-          </div>
-
-          <div>
-            <input
-              type="tel"
-              placeholder="número de telefono"
-              className="w-full p-2 mb-2 border rounded"
-            />
-          </div>
-
-          <div>
-            <input
-              type="text"
-              placeholder="Ubicación"
-              className="w-full p-2 mb-2 border rounded"
-            />
-          </div>
-
-          <div className="justify-center text-center">
-            <p>Intereses:</p>
-            <button className="px-4 py-2 text-white bg-yellow-300 rounded-3xl">
-              Java Script
+          <div className="flex items-center justify-center gap-6 mt-4 text-xs md:text-base top-16">
+            <button
+              type="button"
+              className="flex mt-3 rounded-3xl justify-center bg-orangeprimary px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 border-2 border-white"
+              style={{ boxShadow: "0px 4px 10px 0px #00000040" }}
+            >
+              <Link to="/event-list">Volver a los eventos</Link>
             </button>
-            <button className="px-4 py-2 text-white rounded-3xl bg-lime-100">
-              Python
-            </button>
-            <button className="px-4 py-2 text-white rounded-3xl bg-sky-300">
-              Raect
-            </button>
-            <button className="px-4 py-2 text-white bg-purple-200 rounded-3xl">
-              Tailwind
-            </button>
-          </div>
 
-          <div className="text-center">
-            <button className="px-4 py-2 mt-10 text-white rounded-3xl bg-orangeprimary">
-              Guardar
+            <button
+              type="button"
+              className="flex mt-3 rounded-3xl justify-center bg-orangeprimary px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 border-2 border-white"
+              style={{ boxShadow: "0px 4px 10px 0px #00000040" }}
+            >
+              <Link to="/perfil-edit">¿Deseas editar tu perfil?</Link>
             </button>
           </div>
         </div>
